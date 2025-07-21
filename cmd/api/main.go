5fd0154703e9780a4cbbdecfee180b819457236a
far1h <farihmuhammad45@gmail.com>
2025-07-21 08:47:30 +0700

    Adding a route to our handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,10 +23,8 @@ func main() {
 
 	log.Println("Starting server on port", port)
 
-	http.HandleFunc("/", Hello)
-
 	// start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal("Error starting server: ", err)
 	}
